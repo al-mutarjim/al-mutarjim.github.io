33ead32883f63c4d21b1d2dc42a1487918e6018e
Colin Brayton <gringolalia@gmail.com>
2019-11-12 15:15:37 -0300

    deploy

---
+++ precache-manifest.f66b4f9a876b29efba7e3880b0ec36a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2177e6f220095b23aa6c",
+    "url": "/js/about.9179d50c.js"
+  },
+  {
+    "revision": "41856e10f5a93a139acf",
+    "url": "/css/app.8359d12f.css"
+  },
+  {
+    "revision": "41856e10f5a93a139acf",
+    "url": "/js/app.a7ed0f40.js"
+  },
+  {
+    "revision": "c8bb6542b8b975aabc09",
+    "url": "/js/chunk-vendors.f80cbe3d.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d32f14fc1a10b7627aa2aed36f1b8265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
